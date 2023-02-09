c030c4a18c5f2c4183effce31101015da655fdfc
Martin Fromont <62103174+mart1fromont@users.noreply.github.com>
2023-02-09 11:03:31 +0100

    Added test for backstage_passes

--- gilded_rose.py
+++ gilded_rose.py
@@ -40,7 +40,7 @@ class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
-        self.quality = quality
+        self.quality = quality if self.name == "Sulfuras, Hand of Ragnaros" or quality < 50 else 50
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -9,29 +9,41 @@ class GildedRoseTest(unittest.TestCase):
     This is a test class for the Gilded Rose Kata
     """
 
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
-
     def test_quality_never_over_50(self):
         """
         Test that quality is never over 50
         """
-        items = [Item("test_product", 0, 50)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
+        items = [Item("test_product", 0, 60)]
         self.assertEquals(50, items[0].quality)
 
     def test_sulfuras_quality_never_changes(self):
         """
         Test that Sulfuras quality never changes
         """
-        items = [Item("test_sulfuras", 0, 80)]
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(80, items[0].quality)
+        self.assertEqual(80, items[0].quality)
+
+    def test_backstage_passes_product(self):
+        """
+        Test that backstage passes quality increases by 2 when sell_in is 10 or less, and by 3 when sell_in is 5 or less
+        Also check that quality is 0 when sell_in is 0
+        """
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(22, items[0].quality)
+
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(23, items[0].quality)
+
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(0, items[0].quality)
 
 
 if __name__ == '__main__':
