28aca029ddc1a32785ad456986053a215a0c80b4
Martin Fromont <62103174+mart1fromont@users.noreply.github.com>
2023-02-09 10:50:26 +0100

    Added test for quality never over 50

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,11 +5,26 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
+    """
+    This is a test class for the Gilded Rose Kata
+    """
+
     def test_foo(self):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
+
+    def test_quality_never_over_50(self):
+        """
+        Test that quality is never over 50
+        """
+        items = [Item("test_product", 0, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(50, items[0].quality)
+
+
 
 
 if __name__ == '__main__':
